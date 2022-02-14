47255cc33a034033caf65c80c5061cf4a09b0430
Georg Pietrek <georg.pietrek@conciso.de>
2022-02-14 18:33:46 +0100

    add calculator endpoint

---
+++ step0110/rest-api/src/main/java/de/conciso/starter/CalculatorRepresentation.java
@@ -0,0 +1,12 @@
+package de.conciso.starter;
+
+public class CalculatorRepresentation {
+
+    public double firstNumber;
+
+    public double secondNumber;
+
+    public double result;
+
+    public char currentOperator;
+}
