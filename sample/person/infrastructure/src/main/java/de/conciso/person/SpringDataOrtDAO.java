837e4d86555b31179e689fa185542fc4a01e90ea
Georg Pietrek <georg.pietrek@conciso.de>
2022-12-30 17:43:00 +0100

    restore person (remove waste)

--- sample/person/infrastructure/src/main/java/de/conciso/person/SpringDataOrtDAO.java
+++
@@ -1,6 +0,0 @@
-package de.conciso.person;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface SpringDataPersonDAO extends PersonDAO, CrudRepository<Person, Integer> {
-}
